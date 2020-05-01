6fa6c726adcafa46d2ad7f0ce9147f9532f4e1da
Gary Yu <gong_yu@comcast.com>
2020-05-01 18:01:17 -0400

    time check report

--- time_check.sh
+++ time_check.sh
@@ -3,7 +3,8 @@ start_time=$(awk -F= '/start_time=/ {print $2}' version-timewindow.txt)
 end_time=$(awk -F= '/end_time=/ {print $2}' version-timewindow.txt)
 now=$(date +"%Y/%m/%d_%T")
 
-echo $start_time, $now, $end_time
+echo "Start,              Now,                   End"
+echo "$start_time,   $now,   $end_time"
 
 if [[ "$start_time" < "$now" ]] && [[ "$now" < "$end_time" ]]; then
     echo "Task is now IN the time window."
